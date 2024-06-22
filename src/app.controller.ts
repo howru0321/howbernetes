0d15f49baedb1dce63eab7cde7654f779f82be5d
Hawoo Jung <howru0321@postech.ac.kr>
2024-06-23 00:11:13 +0900

    setup /deploy and /run

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as yaml from 'js-yaml';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,26 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/deploy')
+  deploy(@Body() body: { data: string }): string {
+    try {
+      const config = yaml.load(body.data);
+      console.log('Received deployment configuration:', config);
+      // 여기에 실제 배포 로직을 추가합니다.
+      return 'Deployment configuration received successfully';
+    } catch (error) {
+      console.error('Error parsing the YAML file:', error);
+      throw new Error('Invalid YAML file');
+    }
+  }
+
+  @Post('/run')
+  async runContainer(@Body() body: {container: string, image: number }) {
+    const { container, image } = body;
+    console.log(`Container: ${container}, Image: ${image}`);
+
+    return `Container ${container} with image ${image} is being processed`
   }
 }
