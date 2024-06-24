17afa7fb0521542f8da78856541e45563a69b701
Hawoo Jung <howru0321@postech.ac.kr>
2024-06-24 19:15:37 +0900

    setup howbelet

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('run-container')
+  async runContainer(@Body() body:{container : string, image : string}) {
+    const {container, image} = body;
+
+    
+    const containerMetadata : string = await this.appService.runContainer(container, image);
+    return containerMetadata;
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { exec } from 'child_process';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+runContainer(containerName: string, imageName: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const command = `docker run -d --name ${containerName} ${imageName}`;
+      
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${stderr}`);
+      } else {
+        const containerId = stdout.trim();
+        
+        const inspectCommand = `docker inspect ${containerId}`;
+        
+        exec(inspectCommand, (inspectError, inspectStdout, inspectStderr) => {
+          if (inspectError) {
+            reject(`Error inspecting container: ${inspectStderr}`);
+          } else {
+            //resolve(inspectStdout);
+            return inspectStdout;
+          }
+        });
+      }
+    });
+  });
+}
+
+
+}
